8cf757c4450bdb2e5d723debeb62c5f360666e99
Pawel <synk@poczta.onet.pl>
2022-04-05 20:43:32 +0200

    Add code for Filters lesson
    Lesson showing to filter HTTP request.
    This can be used for example for authentication

---
+++ myJakarta/filters/src/main/java/pl/pdob/controller/SecretController.java
@@ -0,0 +1,15 @@
+package pl.pdob.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/secret")
+public class SecretController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().println("Java jest super");
+    }
+}
---
+++ myJakarta/filters/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Sekretna strona</title>
+</head>
+<body>
+    <form action="${pageContext.request.contextPath}/secret" method="post">
+        <label for="pass">Hasło</label>
+        <input type="password" name="pass" id="pass">
+        <button type="submit">Wyślij</button>
+    </form>
+</body>
+</html>
